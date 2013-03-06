cf4b9c08c6711400ae397441e31ea107bd9cfaae
Christian Kvalheim <christkv@gmail.com>
2013-03-06 14:33:38 +0100

    Split out classes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Configuration = require('./lib/configuration').Configuration
   , Runner = require('./lib/runner').Runner
-  , ParallelRunner = require('./lib/runner').ParallelRunner;
+  , ParallelRunner = require('./lib/parallel_runner').ParallelRunner;
 
 exports.Configuration = Configuration;
 exports.Runner = Runner;
