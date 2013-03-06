fd4755d178091b08f3273df54af98a052daaae09
Christian Kvalheim <christkv@gmail.com>
2013-03-06 12:58:37 +0100

    Added better support for parallel running tests

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Configuration = require('./lib/configuration').Configuration
-  , Runner = require('./lib/runner').Runner;
+  , Runner = require('./lib/runner').Runner
+  , ParallelRunner = require('./lib/runner').ParallelRunner;
 
 exports.Configuration = Configuration;
 exports.Runner = Runner;
+exports.ParallelRunner = ParallelRunner;
