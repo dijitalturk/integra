d2ce3b13c5e44f93701d6c1bf7b5692f80e1fc26
Christian Kvalheim <christkv@gmail.com>
2014-02-27 14:38:59 +0100

    Fixed output and added summary field

--- test/instrumentor_tests.js
+++ test/instrumentor_tests.js
@@ -2,6 +2,7 @@ var Cursor = require('./coverage/cursor').Cursor;
 
 exports['Exercise the cursor code'] = function(configuration, test) {
 	new Cursor([1, 2, 3]).length();
+	
 	// var cursor = new Cursor([1, 2, 3, 4]);
 	// test.equal(4, cursor.length());
 	// test.equal(1, cursor.next());
